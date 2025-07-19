a06a6ae401742e918ad73ec44ffaaf5ee7d2a0d4
slkagura <slkagura@foxmail.com>
2025-07-19 13:38:06 +0800

    <feature>优化项目配置

--- game/build.gradle.kts
+++ game/build.gradle.kts
@@ -1,14 +1,2 @@
 group = "org.example.redalert"
 version = "unspecified"
-
-dependencies {
-    testImplementation(libs.kotlin.test)
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(libs.versions.java.get().toInt())
-}
